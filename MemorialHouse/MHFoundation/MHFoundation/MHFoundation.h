aba40e15bec38c14da8471a24eec74a111dfa6ac
yuncheol-AHN <anchul@hanmail.net>
2024-11-05 11:09:01 +0900

    feat: MHFoundation 모듈 생성

---
+++ MemorialHouse/MHFoundation/MHFoundation/MHFoundation.h
@@ -0,0 +1,18 @@
+//
+//  MHFoundation.h
+//  MHFoundation
+//
+//  Created by 안윤철 on 11/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MHFoundation.
+FOUNDATION_EXPORT double MHFoundationVersionNumber;
+
+//! Project version string for MHFoundation.
+FOUNDATION_EXPORT const unsigned char MHFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHFoundation/PublicHeader.h>
+
+
