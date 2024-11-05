77ff520df7779893caaacd7db8715f6eeb267665
yuncheol-AHN <70050038+yuncheol-AHN@users.noreply.github.com>
2024-11-05 11:22:13 +0900

    Update MemorialHouse/MHFoundation/MHFoundation/MHFoundation.h
    Co-authored-by: 박효준 <gywns626@naver.com>

--- MemorialHouse/MHFoundation/MHFoundation/MHFoundation.h
+++ MemorialHouse/MHFoundation/MHFoundation/MHFoundation.h
@@ -7,12 +7,7 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for MHFoundation.
 FOUNDATION_EXPORT double MHFoundationVersionNumber;
-
-//! Project version string for MHFoundation.
 FOUNDATION_EXPORT const unsigned char MHFoundationVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <MHFoundation/PublicHeader.h>
 
 
