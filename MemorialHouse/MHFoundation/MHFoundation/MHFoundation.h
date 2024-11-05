571ad329f18d9aa98238aaafc6ef68a64cae2637
Kyxxn <gywns626@naver.com>
2024-11-05 17:10:06 +0900

    chore: 헤더파일 주석 제거

--- MemorialHouse/MHFoundation/MHFoundation/MHFoundation.h
+++ MemorialHouse/MHFoundation/MHFoundation/MHFoundation.h
@@ -1,13 +1,4 @@
-//
-//  MHFoundation.h
-//  MHFoundation
-//
-//  Created by 안윤철 on 11/5/24.
-//
-
 #import <Foundation/Foundation.h>
 
 FOUNDATION_EXPORT double MHFoundationVersionNumber;
 FOUNDATION_EXPORT const unsigned char MHFoundationVersionString[];
-
-
