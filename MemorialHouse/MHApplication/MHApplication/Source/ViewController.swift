836d52ff64dab8d9551788bc2383eb9bb4f30cc4
yuncheol-AHN <anchul@hanmail.net>
2024-11-06 00:37:47 +0900

    t

--- MemorialHouse/MHApplication/MHApplication/Source/ViewController.swift
+++ MemorialHouse/MHApplication/MHApplication/Source/ViewController.swift
@@ -5,11 +5,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        print("hello")
         view.backgroundColor = .red
-        let numb = [1, 2, 3,
-                    4, 5, 6]
-        @available(iOS 11.0, *)
-        func hihi() { }
     }
 }
