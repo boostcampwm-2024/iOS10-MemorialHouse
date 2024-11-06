88fc45ee194a3fda212c1ae6ba3b396ea60e156c
Kyxxn <gywns626@naver.com>
2024-11-06 18:54:23 +0900

    feat: 온글잎 베리려원 폰트에 대한 편의 메소드 구현

---
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Extensions/UIFont+Ownglyph.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension UIFont {
+    static func ownglyphBerry(size: CGFloat) -> UIFont {
+        return UIFont(name: "Ownglyph_BERRY_RW-Rg", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+}
