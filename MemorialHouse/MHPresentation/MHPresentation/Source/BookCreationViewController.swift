368dbed8da5d7b575758e99143a2aab42cf47ede
iceHood <hanyo3477@gmail.com>
2024-11-08 19:27:38 +0900

    refactor: 뷰 놓는 함수들 분리

--- MemorialHouse/MHPresentation/MHPresentation/Source/BookCreationViewController.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/BookCreationViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 public final class BookCreationViewController: UIViewController {
     // MARK: - Property
+    private let bookPreviewViewBackground = UIView()
     private let bookPreview: UIView = {
         let view = UIView()
         view.backgroundColor = .clear
@@ -20,13 +21,14 @@ public final class BookCreationViewController: UIViewController {
         
         return imageView
     }()
+    private let bookTitleTextFieldBackground = UIView()
     private let bookTitleTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "책 제목을 입력하세요"
         
         let leftView = UILabel(style: .default)
         
-        leftView.text = " 책 제목 | "
+        leftView.text = "책 제목 | "
         textField.leftView = leftView
         textField.leftViewMode = .always
         
@@ -37,6 +39,14 @@ public final class BookCreationViewController: UIViewController {
         
         return textField
     }()
+    private let bookColorSelectionBackground = UIView()
+    private let bookColorSelectionTitleLabel: UIView = {
+        let view = UILabel(style: .default)
+        view.text = "책 색상 | "
+        view.textAlignment = .left
+        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return view
+    }()
     private let bookColorSelectionView: UIView = {
         let view = UIView()
         
@@ -46,6 +56,31 @@ public final class BookCreationViewController: UIViewController {
         view.clipsToBounds = true
         
         return view
+    }()
+    private let firstLineColorButtonStackView: UIStackView = {
+        let firstLine = UIStackView()
+        firstLine.axis = .horizontal
+        firstLine.distribution = .fillEqually
+        firstLine.alignment = .center
+        firstLine.spacing = 13
+        
+        return firstLine
+    }()
+    private let secondLineColorButtonStackView: UIStackView = {
+        let secondLine = UIStackView()
+        secondLine.axis = .horizontal
+        secondLine.distribution = .fillEqually
+        secondLine.alignment = .center
+        secondLine.spacing = 13
+        
+        return secondLine
+    }()
+    private let colorButtonStackView: UIStackView = {
+        let entireStackView = UIStackView()
+        entireStackView.axis = .vertical
+        entireStackView.distribution = .fillEqually
+        entireStackView.alignment = .fill
+        return entireStackView
     }()
     private let bookColorButtons: [UIButton] = zip(
         ["분홍", "초록", "파랑", "주황", "베이지", nil],
@@ -61,9 +96,10 @@ public final class BookCreationViewController: UIViewController {
         
         return button
     }
+    private let categorySelectionButtonBackground = UIView()
     private let categorySelectionButton: UIButton = {
         let button = UIButton(type: .custom)
-        button.setTitle(" 카테고리 선택 | 없음", for: .normal)
+        button.setTitle("카테고리 선택 | 없음", for: .normal)
         button.setTitleColor(.title, for: .normal)
         button.contentHorizontalAlignment = .left
         
@@ -76,6 +112,7 @@ public final class BookCreationViewController: UIViewController {
         
         return button
     }()
+    private let imageSelectionButtonBackground = UIView()
     private let imageSelectionButton: UIButton = {
         let button = UIButton()
         button.setTitle("사진 선택", for: .normal)
@@ -98,109 +135,105 @@ public final class BookCreationViewController: UIViewController {
         super.viewDidLoad()
         
         configureHirachy()
+        configureLayout()
     }
     
     // MARK: - Helper
     private func configureHirachy() {
         // 리사이즈 마스크 사용 안함
+        bookPreviewViewBackground.translatesAutoresizingMaskIntoConstraints = false
         bookPreview.translatesAutoresizingMaskIntoConstraints = false
         bookImageView.translatesAutoresizingMaskIntoConstraints = false
+        bookTitleTextFieldBackground.translatesAutoresizingMaskIntoConstraints = false
         bookTitleTextField.translatesAutoresizingMaskIntoConstraints = false
+        bookColorSelectionBackground.translatesAutoresizingMaskIntoConstraints = false
+        bookColorSelectionTitleLabel.translatesAutoresizingMaskIntoConstraints = false
         bookColorSelectionView.translatesAutoresizingMaskIntoConstraints = false
+        firstLineColorButtonStackView.translatesAutoresizingMaskIntoConstraints = false
+        secondLineColorButtonStackView.translatesAutoresizingMaskIntoConstraints = false
+        colorButtonStackView.translatesAutoresizingMaskIntoConstraints = false
         bookColorButtons.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        categorySelectionButtonBackground.translatesAutoresizingMaskIntoConstraints = false
         categorySelectionButton.translatesAutoresizingMaskIntoConstraints = false
+        imageSelectionButtonBackground.translatesAutoresizingMaskIntoConstraints = false
         imageSelectionButton.translatesAutoresizingMaskIntoConstraints = false
         
         // 책 미리보기
-        let bookPreviewViewBackground = addBackgroundView(to: bookPreview)
-        view.addSubview(bookPreviewViewBackground)
+        configureBackgroundView(bookPreviewViewBackground, withContent: bookPreview)
+        
+        // 책 이미지
+        bookPreview.addSubview(bookImageView)
+        
+        // 책 제목
+        configureBackgroundView(bookTitleTextFieldBackground, withContent: bookTitleTextField)
+        
+        // 책 색상 타이틀
+        configureBackgroundView(bookColorSelectionBackground, withContent: bookColorSelectionView)
+        bookColorSelectionView.addSubview(bookColorSelectionTitleLabel)
+        
+        // 책 색상 버튼
+        bookColorSelectionView.addSubview(colorButtonStackView)
+        (0..<3).forEach { firstLineColorButtonStackView.addArrangedSubview(bookColorButtons[$0]) }
+        (3..<6).forEach { secondLineColorButtonStackView.addArrangedSubview(bookColorButtons[$0]) }
+        colorButtonStackView.addArrangedSubview(firstLineColorButtonStackView)
+        colorButtonStackView.addArrangedSubview(secondLineColorButtonStackView)
+
+        // 카테고리
+        configureBackgroundView(categorySelectionButtonBackground, withContent: categorySelectionButton)
+        
+        // 사진선택
+        configureBackgroundView(imageSelectionButtonBackground, withContent: imageSelectionButton)
+    }
+    private func configureLayout() {
+        // 책 프리뷰 배경
         NSLayoutConstraint.activate([
             bookPreviewViewBackground.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             bookPreviewViewBackground.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
             bookPreviewViewBackground.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             bookPreviewViewBackground.heightAnchor.constraint(equalToConstant: 344)
         ])
-        
         // 책 이미지
-        bookPreview.addSubview(bookImageView)
         NSLayoutConstraint.activate([
             bookImageView.leadingAnchor.constraint(equalTo: bookPreview.leadingAnchor, constant: 100),
             bookImageView.trailingAnchor.constraint(equalTo: bookPreview.trailingAnchor, constant: -100),
             bookImageView.topAnchor.constraint(equalTo: bookPreview.topAnchor, constant: 70),
             bookImageView.bottomAnchor.constraint(equalTo: bookPreview.bottomAnchor, constant: -70)
         ])
-        
-        // 책 제목
-        let bookTitleTextFieldBackground = addBackgroundView(to: bookTitleTextField)
-        view.addSubview(bookTitleTextFieldBackground)
+        // 책 제목 입력
         NSLayoutConstraint.activate([
             bookTitleTextFieldBackground.leadingAnchor.constraint(equalTo: bookPreviewViewBackground.leadingAnchor),
             bookTitleTextFieldBackground.trailingAnchor.constraint(equalTo: bookPreviewViewBackground.trailingAnchor),
             bookTitleTextFieldBackground.topAnchor.constraint(equalTo: bookPreviewViewBackground.bottomAnchor, constant: 12),
             bookTitleTextFieldBackground.heightAnchor.constraint(equalToConstant: 63)
         ])
-        
-        // 책 색상 타이틀
-        let bookColorSelectionBackground = addBackgroundView(to: bookColorSelectionView)
-        let bookColorSelectionTitleLabel = UILabel(style: .default)
-        bookColorSelectionTitleLabel.text = "책 색상 | "
-        bookColorSelectionTitleLabel.textAlignment = .left
-        bookColorSelectionTitleLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-        bookColorSelectionView.addSubview(bookColorSelectionTitleLabel)
+        // 책 색 선택 왼쪽 타이틀
         NSLayoutConstraint.activate([
             bookColorSelectionTitleLabel.topAnchor.constraint(equalTo: bookColorSelectionView.topAnchor),
             bookColorSelectionTitleLabel.leadingAnchor.constraint(equalTo: bookColorSelectionView.leadingAnchor, constant: 10),
             bookColorSelectionTitleLabel.bottomAnchor.constraint(equalTo: bookColorSelectionView.bottomAnchor)
         ])
-        
-        // 책 버튼
-        let firstLineColorButtons = UIStackView()
-        (0..<3).forEach { firstLineColorButtons.addArrangedSubview(bookColorButtons[$0]) }
-        firstLineColorButtons.axis = .horizontal
-        firstLineColorButtons.distribution = .fillEqually
-        firstLineColorButtons.alignment = .center
-        firstLineColorButtons.spacing = 13
-        let secondLineColorButtons = UIStackView()
-        (3..<6).forEach { secondLineColorButtons.addArrangedSubview(bookColorButtons[$0]) }
-        secondLineColorButtons.axis = .horizontal
-        secondLineColorButtons.distribution = .fillEqually
-        secondLineColorButtons.alignment = .center
-        secondLineColorButtons.spacing = 13
-        let colorButtonsStackView = UIStackView(arrangedSubviews: [firstLineColorButtons, secondLineColorButtons])
-        colorButtonsStackView.axis = .vertical
-        colorButtonsStackView.distribution = .fillEqually
-        colorButtonsStackView.alignment = .fill
-        
-        bookColorSelectionBackground.addSubview(colorButtonsStackView)
-        colorButtonsStackView.translatesAutoresizingMaskIntoConstraints = false
+        // 책 버튼들
         NSLayoutConstraint.activate([
-            colorButtonsStackView.leadingAnchor.constraint(equalTo: bookColorSelectionTitleLabel.trailingAnchor, constant: 20),
-            colorButtonsStackView.trailingAnchor.constraint(equalTo: bookColorSelectionView.trailingAnchor, constant: -18),
-            colorButtonsStackView.topAnchor.constraint(equalTo: bookColorSelectionView.topAnchor, constant: 5),
-            colorButtonsStackView.bottomAnchor.constraint(equalTo: bookColorSelectionView.bottomAnchor, constant: -5)
+            colorButtonStackView.leadingAnchor.constraint(equalTo: bookColorSelectionTitleLabel.trailingAnchor, constant: 20),
+            colorButtonStackView.trailingAnchor.constraint(equalTo: bookColorSelectionView.trailingAnchor, constant: -18),
+            colorButtonStackView.topAnchor.constraint(equalTo: bookColorSelectionView.topAnchor, constant: 5),
+            colorButtonStackView.bottomAnchor.constraint(equalTo: bookColorSelectionView.bottomAnchor, constant: -5)
         ])
-        
-        view.addSubview(bookColorSelectionBackground)
+        // 책 색 선택 배경
         NSLayoutConstraint.activate([
             bookColorSelectionBackground.leadingAnchor.constraint(equalTo: bookTitleTextFieldBackground.leadingAnchor),
             bookColorSelectionBackground.trailingAnchor.constraint(equalTo: bookTitleTextFieldBackground.trailingAnchor),
             bookColorSelectionBackground.topAnchor.constraint(equalTo: bookTitleTextFieldBackground.bottomAnchor, constant: 8),
             bookColorSelectionBackground.heightAnchor.constraint(equalToConstant: 113)
         ])
-        
-        // 카테고리
-        let categorySelectionButtonBackground = addBackgroundView(to: categorySelectionButton)
-        view.addSubview(categorySelectionButtonBackground)
+        // 카테고리 선택 배경
         NSLayoutConstraint.activate([
             categorySelectionButtonBackground.leadingAnchor.constraint(equalTo: bookColorSelectionBackground.leadingAnchor),
             categorySelectionButtonBackground.trailingAnchor.constraint(equalTo: bookColorSelectionBackground.trailingAnchor),
             categorySelectionButtonBackground.topAnchor.constraint(equalTo: bookColorSelectionBackground.bottomAnchor, constant: 8),
             categorySelectionButtonBackground.heightAnchor.constraint(equalToConstant: 63)
         ])
-        
-        // 사진선택
-        let imageSelectionButtonBackground = addBackgroundView(to: imageSelectionButton)
-        view.addSubview(imageSelectionButtonBackground)
+        // 이미지 선택 배경
         NSLayoutConstraint.activate([
             imageSelectionButtonBackground.leadingAnchor.constraint(equalTo: categorySelectionButtonBackground.leadingAnchor),
             imageSelectionButtonBackground.trailingAnchor.constraint(equalTo: categorySelectionButtonBackground.trailingAnchor),
@@ -208,16 +241,12 @@ public final class BookCreationViewController: UIViewController {
             imageSelectionButtonBackground.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -27),
             imageSelectionButtonBackground.heightAnchor.constraint(equalToConstant: 63)
         ])
-        
     }
-    private func addBackgroundView(to view: UIView) -> UIView {
-        let backGroundView = UIView()
-        backGroundView.translatesAutoresizingMaskIntoConstraints = false
+    private func configureBackgroundView(_ backGroundView: UIView, withContent view: UIView) {
         backGroundView.backgroundColor = .mhSection
         backGroundView.layer.cornerRadius = 20
         backGroundView.clipsToBounds = true
         backGroundView.addSubview(view)
-        view.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             view.leadingAnchor.constraint(equalTo: backGroundView.leadingAnchor, constant: 6),
             view.trailingAnchor.constraint(equalTo: backGroundView.trailingAnchor, constant: -6),
@@ -225,6 +254,6 @@ public final class BookCreationViewController: UIViewController {
             view.bottomAnchor.constraint(equalTo: backGroundView.bottomAnchor, constant: -7)
         ])
         
-        return backGroundView
+        self.view.addSubview(backGroundView)
     }
 }
