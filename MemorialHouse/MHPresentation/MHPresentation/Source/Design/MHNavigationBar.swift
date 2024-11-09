7650d902e817a61637753b36c19c1ccaac2ffebe
Kyxxn <gywns626@naver.com>
2024-11-10 00:33:56 +0900

    fix: 네비게이션 바 이미지를 Button으로 변경

--- MemorialHouse/MHPresentation/MHPresentation/Source/Design/MHNavigationBar.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Design/MHNavigationBar.swift
@@ -3,41 +3,55 @@ import UIKit
 final class MHNavigationBar: UIView {
     // MARK: - Property
     private let titleLabel = UILabel(style: .header)
-    private let settingImageView = UIImageView(image: .settingLight)
+    private let settingButton = UIButton(type: .custom)
     
     // MARK: - Initializer
     init(title: String) {
         super.init(frame: .zero)
         
         setup(with: title)
+        configureAddSubView()
         configureConstraints()
+        configureAction()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         
         setup(with: "")
+        configureAddSubView()
         configureConstraints()
+        configureAction()
     }
     
     // MARK: - Setup & Configuration
-    func setup(with title: String) {
+    private func setup(with title: String) {
         titleLabel.text = "\(title) 기록소"
+        settingButton.setImage(.settingLight, for: .normal)
         backgroundColor = .baseBackground
-        addSubview(titleLabel)
-        addSubview(settingImageView)
     }
     
-    func configureConstraints() {
+    private func configureAddSubView() {
+        addSubview(titleLabel)
+        addSubview(settingButton)
+    }
+    
+    private func configureConstraints() {
         titleLabel.setTop(anchor: topAnchor)
         titleLabel.setLeading(anchor: leadingAnchor)
         titleLabel.setBottom(anchor: bottomAnchor)
-        titleLabel.setCenterY(view: settingImageView)
+        titleLabel.setCenterY(view: settingButton)
         
-        settingImageView.setTop(anchor: topAnchor)
-        settingImageView.setTrailing(anchor: trailingAnchor)
-        settingImageView.setCenterY(view: self)
-        settingImageView.setWidth(30)
-        settingImageView.setHeight(30)
+        settingButton.setTop(anchor: topAnchor)
+        settingButton.setTrailing(anchor: trailingAnchor)
+        settingButton.setCenterY(view: self)
+        settingButton.setWidth(30)
+        settingButton.setHeight(30)
+    }
+    
+    private func configureAction() {
+        settingButton.addAction(UIAction { _ in
+            // TODO: 설정 버튼 클릭 시 설정화면 라우팅 필요
+        }, for: .touchUpInside)
     }
 }
