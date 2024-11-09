e6615b39d05a6865fb679c9636162cfd34a7deaa
k2645 <k2645@naver.com>
2024-11-09 19:58:45 +0900

    feat: 구분선 View 생성 extension 구현

---
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Extensions/UIView+DividedLine.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIView {
+    static func dividedLine() -> UIView {
+        let line = UIView()
+        line.backgroundColor = .dividedLine
+        line.setHeight(1)
+        
+        return line
+    }
+}
