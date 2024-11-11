8e4e69bd4f44841ca99f8f62a85123515838c206
Kyxxn <gywns626@naver.com>
2024-11-12 04:05:44 +0900

    feat: MHBook 완성
    컨테이너 뷰 안에 아래의 내용을 넣었습니다
    - 책 겉면 (대영현 디자인)
    - 책 제목
    - 대상 이미지
    - 유저 출판소 이름

--- MemorialHouse/MHPresentation/MHPresentation/Source/Design/MHBook.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Design/MHBook.swift
@@ -16,3 +16,69 @@ extension BookType {
         }
     }
 }
+
+final class MHBook: UIView {
+    // MARK: - Property
+    private let containerView = UIView()
+    private let bookImageView = UIImageView()
+    private let titleLabel = UILabel(style: .default)
+    private let targetImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "person"))
+        imageView.contentMode = .scaleToFill
+        
+        imageView.layer.shadowRadius = 4
+        imageView.layer.shadowOpacity = 0.4
+        imageView.layer.shadowOffset = CGSize(width: 4, height: 4)
+        
+        return imageView
+    }()
+    private let publisherLabel = UILabel(style: .body2)
+    
+    // MARK: - Initializer
+    init() {
+        super.init(frame: .zero)
+        
+        configureAddSubView()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        configureAddSubView()
+        configureConstraints()
+    }
+    
+    // MARK: - Configuration
+    func configure(with bookCover: BookCover) {
+        titleLabel.text = bookCover.title
+        bookImageView.image = bookCover.bookType.image
+        targetImageView.image = UIImage(systemName: "person")
+        if let publisher = UserDefaults.standard.object(forKey: Constant.houseNameUserDefaultKey) as? String {
+            // TODO: User 모델 만들어지면 파라미터로 출판소 이름 넘겨주기
+            publisherLabel.text = publisher
+        }
+    }
+    
+    private func configureAddSubView() {
+        addSubview(containerView)
+        containerView.addSubview(bookImageView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(targetImageView)
+        containerView.addSubview(publisherLabel)
+    }
+    
+    private func configureConstraints() {
+        containerView.fillSuperview()
+        bookImageView.fillSuperview()
+        titleLabel.setTop(anchor: containerView.topAnchor, constant: 16)
+        titleLabel.setCenterX(view: containerView, constant: 8)
+        targetImageView.setTop(anchor: titleLabel.bottomAnchor, constant: 14)
+        targetImageView.setCenterX(view: containerView, constant: 8)
+        targetImageView.setWidthAndHeight(width: 100, height: 100)
+        publisherLabel.setAnchor(
+            bottom: containerView.bottomAnchor, constantBottom: 12,
+            trailing: containerView.trailingAnchor, constantTrailing: 12
+        )
+    }
+}
