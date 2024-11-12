d519730017cd810e86090481a6c89334ce45117d
Kyxxn <gywns626@naver.com>
2024-11-12 15:11:52 +0900

    refactor: BookCover 엔티티 프로퍼티 추가

--- MemorialHouse/MHDomain/MHDomain/Entity/BookCover.swift
+++ MemorialHouse/MHDomain/MHDomain/Entity/BookCover.swift
@@ -1,16 +1,24 @@
+import MHFoundation
 
 public struct BookCover {
+    public let bookIdentifer = UUID()
     public let title: String
     public let imageURL: String
-    public let bookType: BookType
+    public let bookType: BookColor
+    public let category: String
+    public let favorite: Bool
     
     public init(
         title: String,
         imageURL: String,
-        bookType: BookType
+        bookType: BookColor,
+        category: String,
+        favorite: Bool = false
     ) {
         self.title = title
         self.imageURL = imageURL
         self.bookType = bookType
+        self.category = category
+        self.favorite = favorite
     }
 }
