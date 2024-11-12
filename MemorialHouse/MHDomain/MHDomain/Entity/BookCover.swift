1cd9c3f1a78e62ebdc12af5e717306fa04fd3104
Kyxxn <gywns626@naver.com>
2024-11-12 15:11:05 +0900

    refactor: BookColor 분리

--- MemorialHouse/MHDomain/MHDomain/Entity/BookCover.swift
+++ MemorialHouse/MHDomain/MHDomain/Entity/BookCover.swift
@@ -1,11 +1,3 @@
-// TODO: 지금 피그잼의 BookCover에서 세분화할 필요성이 느껴짐 (MHBook은 딱 아래 정도만 필요한듯)
-public enum BookType {
-    case beige
-    case blue
-    case green
-    case orange
-    case pink
-}
 
 public struct BookCover {
     public let title: String
