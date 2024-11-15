a421884feb29b3f37ca58c0cbb87e79ccd250c08
Kyxxn <gywns626@naver.com>
2024-11-16 04:23:29 +0900

    feat: public 메소드로 변경

--- MemorialHouse/MHCore/MHCore/MHLogger.swift
+++ MemorialHouse/MHCore/MHCore/MHLogger.swift
@@ -1,31 +1,31 @@
 import OSLog
 
 public enum MHLogger {
-    static func debug<T>(_ object: T?) {
+    public static func debug<T>(_ object: T?) {
         #if DEBUG
-        let message = object != nil ? "\(object!)" : "nil"
+        let message = object != nil ? "[Debug] \(object!)" : "[Debug] nil"
         os_log(.debug, "%@", message)
         #endif
     }
     
-    static func info<T>(_ object: T?) {
+    public static func info<T>(_ object: T?) {
         #if DEBUG
-        let message = object != nil ? "\(object!)" : "nil"
+        let message = object != nil ? "[Info] \(object!)" : "[Info] nil"
         os_log(.info, "%@", message)
         #endif
     }
     
-    static func error<T>(_ object: T?) {
+    public static func error<T>(_ object: T?) {
         #if DEBUG
-        let message = object != nil ? "\(object!)" : "nil"
+        let message = object != nil ? "[Error] \(object!)" : "[Error] nil"
         os_log(.error, "%@", message)
         #endif
     }
     
-    static func network<T>(_ object: T?) {
+    public static func network<T>(_ object: T?) {
         #if DEBUG
-        let message = object != nil ? "\(object!)" : "nil"
-        os_log(.debug, "Network Log: %@", message)
+        let message = object != nil ? "[Network] \(object!)" : "[Network] nil"
+        os_log(.debug, "%@", message)
         #endif
     }
 }
