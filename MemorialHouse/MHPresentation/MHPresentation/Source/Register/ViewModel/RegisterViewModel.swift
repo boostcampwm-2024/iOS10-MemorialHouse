e5f23ba90fd742afde53c780e8e9928e98ed4f36
yuncheol-AHN <anchul@hanmail.net>
2024-11-19 00:17:03 +0900

    refactor: MVC -> MVVM & Combine

---
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Register/ViewModel/RegisterViewModel.swift
@@ -0,0 +1,46 @@
+import MHFoundation
+import Combine
+
+final class RegisterViewModel: ViewModelType {
+    enum Input {
+        case registerTextFieldEdited(text: String?)
+        case registerButtonTapped(text: String)
+    }
+    
+    enum Output {
+        case registerButtonEnabled(isEnabled: Bool)
+        case moveToHome(destination: String)
+    }
+    
+    private let output = PassthroughSubject<Output, Never>()
+    private var cancellables = Set<AnyCancellable>()
+    
+    func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
+        input.sink { [weak self] event in
+            switch event {
+            case .registerTextFieldEdited(let text):
+                self?.validateTextField(text: text)
+            case .registerButtonTapped(let text):
+                self?.registerButtonTapped(text: text)
+            }
+        }.store(in: &cancellables)
+        
+        return output.eraseToAnyPublisher()
+    }
+    
+    private func validateTextField(text: String?) {
+        guard let text else {
+            output.send(.registerButtonEnabled(isEnabled: false))
+            return
+        }
+        output.send(.registerButtonEnabled(isEnabled: !text.isEmpty && text.count < 11))
+    }
+    
+    private func registerButtonTapped(text: String) {
+        UserDefaults.standard.set(
+            text,
+            forKey: Constant.houseNameUserDefaultKey
+        )
+        output.send(.moveToHome(destination: text))
+    }
+}
