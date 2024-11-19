9dabdd408d81de420e5f019623c4613036eb156e
Kyxxn <gywns626@naver.com>
2024-11-19 23:53:30 +0900

    feat: 카테고리 뷰모델로부터 더미 데이터 받아와 테이블뷰 그리기

--- MemorialHouse/MHPresentation/MHPresentation/Source/Category/CategoryViewModel.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Category/CategoryViewModel.swift
@@ -1,20 +1,38 @@
 import Combine
+import MHFoundation
 
-final class CategoryViewModel {
+final class CategoryViewModel: ViewModelType {
     enum Input {
         case viewDidLoad
     }
     
-    @Published private(set) var categories: [String] = []
+    enum Output {
+        case fetchedCategories
+    }
     
-    func action(_ input: CategoryViewModel.Input) {
-        switch input {
-        case .viewDidLoad:
-            fetchCategories()
-        }
+    private(set) var dummyData: [String] = []
+    private let output = PassthroughSubject<Output, Never>()
+    private var cancellables = Set<AnyCancellable>()
+    
+    func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
+        input.sink { [weak self] event in
+            switch event {
+            case .viewDidLoad:
+                self?.fetchCategories()
+            }
+        }.store(in: &cancellables)
+        
+        return output.eraseToAnyPublisher()
+    }
+    
+    func calculateSheetHeight() -> CGFloat {
+        let cellHeight = CategoryTableViewCell.height
+        let itemCount = CGFloat(dummyData.count)
+        return (cellHeight * itemCount) + Constant.navigationBarHeight
     }
     
     private func fetchCategories() {
-        categories = ["집주인들", "가족", "친구"]
+        dummyData = ["집주인들", "가족", "친구", "동료", "기타"]
+        output.send(.fetchedCategories)
     }
 }
