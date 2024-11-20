ff2f095980936ce0a50f8db19b6d3fc796fa887f
Kyxxn <gywns626@naver.com>
2024-11-20 23:54:44 +0900

    feat: DIContainer 구현

---
+++ MemorialHouse/MHCore/MHCore/DIContainer.swift
@@ -0,0 +1,20 @@
+public actor DIContainer {
+    public static let shared = DIContainer()
+    private var objects: [String: Any] = [:]
+    
+    private init() {}
+    
+    public func register<T>(_ type: T.Type, object: Any) {
+        let key = String(describing: type)
+        objects[key] = object
+    }
+    
+    public func resolve<T>(_ type: T.Type) throws -> T {
+        let key = String(describing: type)
+        guard let object = objects[key] as? T else {
+            MHLogger.error("\(#function): \(key)에 해당하는 object 없음")
+            throw MHError.DIContainerResolveFailure(key: key)
+        }
+        return object
+    }
+}
