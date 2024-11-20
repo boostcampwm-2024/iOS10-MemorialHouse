511d694dc214f56265b3be6934db61984804fb94
Kyxxn <gywns626@naver.com>
2024-11-20 14:57:31 +0900

    refactor: UIBarButtonItem 편의 생성자에 normal selected 파라미터 추가

--- MemorialHouse/MHPresentation/MHPresentation/Source/Extensions/UIBarButtonItem+Create.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Extensions/UIBarButtonItem+Create.swift
@@ -4,20 +4,24 @@ extension UIBarButtonItem {
     /// UIBarButtonItem 편의 생성자
     /// - Parameters:
     ///   - title: 버튼 제목
-    ///   - fontSize: 제목의 폰트 크기
-    ///   - color: 제목 텍스트 색상
+    ///   - normal: 기본 상태의 속성 (Optional, 기본값 제공 가능)
+    ///   - selected: 선택 상태의 속성 (Optional, 기본값 제공 가능)
     ///   - action: 버튼 클릭 시 실행할 클로저
     convenience init(
         title: String,
-        fontSize: CGFloat,
-        color: UIColor,
+        normal: [NSAttributedString.Key: Any]? = nil,
+        selected: [NSAttributedString.Key: Any]? = nil,
         action: @escaping () -> Void
     ) {
         let uiAction = UIAction { _ in action() }
         self.init(title: title, primaryAction: uiAction)
-        setTitleTextAttributes([
-            .font: UIFont.ownglyphBerry(size: fontSize),
-            .foregroundColor: color
-        ], for: .normal)
+        
+        if let normalAttributes = normal {
+            setTitleTextAttributes(normalAttributes, for: .normal)
+        }
+        
+        if let selectedAttributes = selected {
+            setTitleTextAttributes(selectedAttributes, for: .selected)
+        }
     }
 }
