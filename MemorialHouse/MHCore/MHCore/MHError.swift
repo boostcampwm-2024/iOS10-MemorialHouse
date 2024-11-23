367096c77fdbc3d5eae3e60733cc3f31c7c8b511
k2645 <k2645@naver.com>
2024-11-23 16:06:17 +0900

    feat: MHError 에러 추가

--- MemorialHouse/MHCore/MHCore/MHError.swift
+++ MemorialHouse/MHCore/MHCore/MHError.swift
@@ -2,11 +2,20 @@ import Foundation
 
 public enum MHError: Error, CustomStringConvertible {
     case DIContainerResolveFailure(key: String)
+    case convertDTOFailure
+    case findEntityFailure
+    case saveContextFailure
     
     public var description: String {
         switch self {
         case .DIContainerResolveFailure(let key):
             "\(key)에 대한 dependency resolve 실패"
+        case .convertDTOFailure:
+            "Entity에 대한 DTO 변환 실패"
+        case .findEntityFailure:
+            "Entity 찾기 실패"
+        case .saveContextFailure:
+            "Update된 Context 저장 실패"
         }
     }
 }
