a558b54f283226644a1ecc74b06a82f5c9f36e51
Kyxxn <gywns626@naver.com>
2024-11-24 03:27:06 +0900

    feat: Home과 관련된 DI ViewModel & UseCase & Repo 등록

--- MemorialHouse/MHApplication/MHApplication/Source/App/SceneDelegate.swift
+++ MemorialHouse/MHApplication/MHApplication/Source/App/SceneDelegate.swift
@@ -14,6 +14,8 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        registerDependency()
+        
         var initialViewController: UIViewController = RegisterViewController(viewModel: RegisterViewModel())
         if let houseName = UserDefaults.standard.object(forKey: Constant.houseNameUserDefaultKey) as? String {
             do {
@@ -28,5 +30,30 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let navigationController = UINavigationController(rootViewController: initialViewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+    }
+    
+    func registerDependency() {
+        do {
+            DIContainer.shared.register(
+                UserHouseRepository.self,
+                object: StubUserHouseRepository()
+            )
+            
+            let userHouseRepository = try DIContainer.shared.resolve(UserHouseRepository.self)
+            DIContainer.shared.register(
+                FetchUserHouseUseCase.self,
+                object: DefaultFetchUserHouseUseCase(repository: userHouseRepository)
+            )
+            
+            let fetchUserHouseUseCase = try DIContainer.shared.resolve(FetchUserHouseUseCase.self)
+            DIContainer.shared.register(
+                HomeViewModelFactory.self,
+                object: HomeViewModelFactory(fetchUserHouseUseCase: fetchUserHouseUseCase)
+            )
+        } catch let error as MHError {
+            MHLogger.error("\(error.description)")
+        } catch {
+            MHLogger.error("\(error.localizedDescription)")
+        }
     }
 }
