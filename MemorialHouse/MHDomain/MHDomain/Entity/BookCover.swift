fa688a90640558ea5dd8297d3ee2fcd49d2dde39
Kyxxn <gywns626@naver.com>
2024-11-25 14:24:36 +0900

    refactor: BookCover 모델 변경

--- MemorialHouse/MHDomain/MHDomain/Entity/BookCover.swift
+++ MemorialHouse/MHDomain/MHDomain/Entity/BookCover.swift
@@ -1,20 +1,22 @@
 import MHFoundation
 
 public struct BookCover: Equatable, Sendable {
-    public let identifier = UUID()
+    public let identifier: UUID
     public let title: String
-    public let imageURL: String
+    public let imageURL: String?
     public let color: BookColor
-    public let category: String
+    public let category: String?
     public let favorite: Bool
     
     public init(
+        identifier: UUID = .init(),
         title: String,
-        imageURL: String,
+        imageURL: String?,
         color: BookColor,
-        category: String,
+        category: String?,
         favorite: Bool = false
     ) {
+        self.identifier = identifier
         self.title = title
         self.imageURL = imageURL
         self.color = color
