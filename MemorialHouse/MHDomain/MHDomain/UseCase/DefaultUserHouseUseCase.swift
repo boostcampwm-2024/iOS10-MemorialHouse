5b190806b0bae67f05c9d17f2c110d10b0b46ad7
Kyxxn <gywns626@naver.com>
2024-11-25 14:18:59 +0900

    fix: 2글자인 경우에만 띄어쓰기 하도록 비즈니스 로직 수정

--- MemorialHouse/MHDomain/MHDomain/UseCase/DefaultUserHouseUseCase.swift
+++ MemorialHouse/MHDomain/MHDomain/UseCase/DefaultUserHouseUseCase.swift
@@ -16,13 +16,13 @@ public struct DefaultFetchMemorialHouseUseCase: FetchMemorialHouseUseCase {
         )
     }
     
-    /// 집 이름이 3글자 이상일 경우, 각 글자 사이에 공백을 추가하여 변환합니다.
+    /// 집 이름이 2글자인 경우, 각 글자 사이에 공백을 추가하여 변환합니다.
     ///
     /// - Parameter name: 원본 이름 문자열.
-    /// - Returns: 글자 사이에 공백이 추가된 문자열(3글자 이상인 경우).
-    ///   2글자 이하라면 원본 문자열 그대로 반환합니다.
+    /// - Returns: 글자 사이에 공백이 추가된 문자열(두 글자인 경우).
+    ///   한 글자, 혹은 세 글자 이상라면 원본 문자열 그대로 반환합니다.
     private func transformHouseName(with name: String) -> String {
-        guard name.count > 2 else { return name }
+        guard name.count == 2 else { return name }
         return name.map { String($0) }.joined(separator: " ")
     }
 }
