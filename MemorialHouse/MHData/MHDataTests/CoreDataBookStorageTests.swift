ead3b39ded6c815fec7c78d02b1180d172be00ff
iceHood <hanyo3477@gmail.com>
2024-11-26 16:56:19 +0900

    feat: BookStorageTest 작성

--- MemorialHouse/MHData/MHDataTests/CoreDataBookStorageTests.swift
+++ MemorialHouse/MHData/MHDataTests/CoreDataBookStorageTests.swift
@@ -7,36 +7,66 @@ import Testing
 struct CoreDataBookStorageTests {
     // MARK: - Properties
     private let sut = CoreDataBookStorage(coreDataStorage: MockCoreDataStorage())
-    private static let book = BookDTO(
-        id: UUID(),
-        index: [],
-        pages: [
-            PageDTO(
-                id: UUID(),
-                metadata: [
-                    0: MediaDescriptionDTO(id: UUID(), type: "image")
-                ],
-                text: "first page"
-            ),
-            PageDTO(
-                id: UUID(),
-                metadata: [
-                    0: MediaDescriptionDTO(id: UUID(), type: "video")
-                ],
-                text: "second page"
-            ),
-            PageDTO(
-                id: UUID(),
-                metadata: [
-                    0: MediaDescriptionDTO(id: UUID(), type: "audio")
-                ],
-                text: "third page"
-            )
-        ]
-    )
+    private static let books = [
+        BookDTO(
+            id: UUID(),
+            index: [],
+            pages: [
+                PageDTO(
+                    id: UUID(),
+                    metadata: [
+                        0: MediaDescriptionDTO(id: UUID(), type: "image")
+                    ],
+                    text: "first page"
+                ),
+                PageDTO(
+                    id: UUID(),
+                    metadata: [
+                        0: MediaDescriptionDTO(id: UUID(), type: "video")
+                    ],
+                    text: "second page"
+                ),
+                PageDTO(
+                    id: UUID(),
+                    metadata: [
+                        0: MediaDescriptionDTO(id: UUID(), type: "audio")
+                    ],
+                    text: "third page"
+                )
+            ]
+        ),
+        BookDTO(
+            id: UUID(),
+            index: [],
+            pages: [
+                PageDTO(
+                    id: UUID(),
+                    metadata: [
+                        0: MediaDescriptionDTO(id: UUID(), type: "image")
+                    ],
+                    text: "first page"
+                ),
+                PageDTO(
+                    id: UUID(),
+                    metadata: [
+                        0: MediaDescriptionDTO(id: UUID(), type: "video")
+                    ],
+                    text: "second page"
+                ),
+                PageDTO(
+                    id: UUID(),
+                    metadata: [
+                        0: MediaDescriptionDTO(id: UUID(), type: "audio")
+                    ],
+                    text: "third page"
+                )
+            ]
+        )
+    ]
     
     init() async {
-        _ = await sut.create(data: CoreDataBookStorageTests.book)
+        _ = await sut.create(data: CoreDataBookStorageTests.books[0])
+        _ = await sut.create(data: CoreDataBookStorageTests.books[1])
     }
     
     @Test func test코어데이터에_새로운_Book_객체를_생성한다() async throws {
@@ -85,12 +115,12 @@ struct CoreDataBookStorageTests {
     @Test func test코어데이터에_저장된_Book_객체를_불러온다() async {
         // Arrange
         // Act
-        let result = await sut.fetch(with: CoreDataBookStorageTests.book.id)
+        let result = await sut.fetch(with: CoreDataBookStorageTests.books[0].id)
         
         // Assert
         switch result {
         case .success(let bookResult):
-            #expect(CoreDataBookStorageTests.book.id == bookResult.id)
+            #expect(CoreDataBookStorageTests.books.contains(where: { $0.id == bookResult.id }))
         case .failure(let error):
             #expect(false, "Fetch Book 실패: \(error.localizedDescription)")
         }
@@ -98,7 +128,7 @@ struct CoreDataBookStorageTests {
     
     @Test func test코어데이터에서_특정_UUID값을_가진_Book_데이터를_업데이트한다() async throws {
         // Arrange
-        let oldBook = CoreDataBookStorageTests.book
+        let oldBook = CoreDataBookStorageTests.books[0]
         let newBook = BookDTO(
             id: oldBook.id,
             index: [],
@@ -145,25 +175,25 @@ struct CoreDataBookStorageTests {
             let newBookResult = coreDataBook
             #expect(newBookResult.pages.count != oldBook.pages.count)
         case .failure(let error):
-            #expect(false, "Update BookCover 실패: \(error.localizedDescription)")
+            #expect(false, "Update Book 실패: \(error.localizedDescription)")
         }
     }
     
-    @Test(arguments: [CoreDataBookCoverStorageTests.bookCovers[0].identifier,
-                      CoreDataBookCoverStorageTests.bookCovers[1].identifier,
+    @Test(arguments: [CoreDataBookStorageTests.books[0].id,
+                      CoreDataBookStorageTests.books[1].id,
                       UUID()]
     ) func test코어데이터에서_특정_UUID값을_가진_BookCover_데이터를_삭제한다(_ id: UUID) async throws {
         // Arrange
         // Act
         let result = await sut.delete(with: id)
-        let coreDataBookCovers = try await sut.fetch().get()
+        let coreDataBook = try await sut.fetch(with: id).get()
         
         // Assert
         switch result {
         case .success:
-            #expect(!coreDataBookCovers.contains(where: { $0.identifier == id }))
+            #expect(false, "Delete Book 실패: 삭제된 Book을 찾았습니다.")
         case .failure(let error):
-            #expect(error == MHError.findEntityFailure && !coreDataBookCovers.contains(where: { $0.identifier == id }))
+            #expect(error == MHError.findEntityFailure)
         }
     }
 }
