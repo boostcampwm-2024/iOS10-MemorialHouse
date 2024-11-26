fbc49b238b704102d46137af3e1f045c7da02ed4
iceHood <hanyo3477@gmail.com>
2024-11-26 16:15:50 +0900

    feat: MHError유형 추가

--- MemorialHouse/MHCore/MHCore/MHError.swift
+++ MemorialHouse/MHCore/MHCore/MHError.swift
@@ -5,6 +5,11 @@ public enum MHError: Error, CustomStringConvertible, Equatable {
     case convertDTOFailure
     case findEntityFailure
     case saveContextFailure
+    case directorySettingError
+    case fileCreationError
+    case fileReadingError
+    case fileDeletionError
+    case fileMovingError
     
     public var description: String {
         switch self {
@@ -16,6 +21,16 @@ public enum MHError: Error, CustomStringConvertible, Equatable {
             "Entity 찾기 실패"
         case .saveContextFailure:
             "Update된 Context 저장 실패"
+        case .directorySettingError:
+            "디렉토리 설정 실패"
+        case .fileCreationError:
+            "파일 생성 실패"
+        case .fileReadingError:
+            "파일 읽기 실패"
+        case .fileDeletionError:
+            "파일 삭제 실패"
+        case .fileMovingError:
+            "파일 이동 실패"
         }
     }
 }
