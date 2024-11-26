f995ed3b7641e2d0050f265924db840fc072a470
Kyxxn <gywns626@naver.com>
2024-11-26 17:20:33 +0900

    feat: 카테고리 생성 수정 삭제에 대한 로직 구현

--- MemorialHouse/MHPresentation/MHPresentation/Source/Category/CategoryViewModel.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Category/CategoryViewModel.swift
@@ -1,4 +1,65 @@
-struct CategoryViewModel {
+import Combine
+import MHCore
+
+final class CategoryViewModel: ViewModelType {
+    enum Input {
+        case addCategory(text: String)
+        case updateCategory(index: Int, text: String)
+        case deleteCategory(index: Int)
+    }
+    
+    enum Output {
+        case addedCategory
+        case updatedCategory
+        case deletedCategory
+    }
+    
+    private let output = PassthroughSubject<Output, Never>()
+    private var cancellables = Set<AnyCancellable>()
     private(set) var categories: [String]
     private(set) var currentCategoryIndex: Int
+    
+    init(cancellables: Set<AnyCancellable> = Set<AnyCancellable>(), categories: [String], currentCategoryIndex: Int) {
+        self.cancellables = cancellables
+        self.categories = categories
+        self.currentCategoryIndex = currentCategoryIndex
+    }
+    
+    func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
+        input.sink { [weak self] event in
+            switch event {
+            case .addCategory(let text):
+                self?.addCategory(text: text)
+            case .updateCategory(let index, let text):
+                self?.updateCategory(index: index, text: text)
+            case .deleteCategory(let index):
+                self?.deleteCategory(index: index)
+            }
+        }.store(in: &cancellables)
+        
+        return output.eraseToAnyPublisher()
+    }
+    
+    private func addCategory(text: String) {
+        categories.append(text)
+        output.send(.addedCategory)
+    }
+    
+    private func updateCategory(index: Int, text: String) {
+        guard index >= 0 && index < categories.count else {
+            MHLogger.error("유효하지 않은 인덱스: \(index)")
+            return
+        }
+        categories[index] = text
+        output.send(.updatedCategory)
+    }
+    
+    private func deleteCategory(index: Int) {
+        guard index >= 0 && index < categories.count else {
+            MHLogger.error("유효하지 않은 인덱스: \(index)")
+            return
+        }
+        categories.remove(at: index)
+        output.send(.deletedCategory)
+    }
 }
