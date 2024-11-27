c23d906bbcaec5c660c47bbd1736d2413f37445c
iceHood <hanyo3477@gmail.com>
2024-11-27 22:48:26 +0900

    refactor: Error유형 변경

--- MemorialHouse/MHData/MHData/LocalStorage/BookCoverStorage.swift
+++ MemorialHouse/MHData/MHData/LocalStorage/BookCoverStorage.swift
@@ -2,7 +2,7 @@ import MHFoundation
 import MHCore
 
 public protocol BookCoverStorage {
-    func create(data: BookCoverDTO) async -> Result<Void, MHCoreError>
+    func create(data: BookCoverDTO) async -> Result<Void, MHDataError>
     func fetch() async -> Result<[BookCoverDTO], MHDataError>
     func update(with id: UUID, data: BookCoverDTO) async -> Result<Void, MHDataError>
     func delete(with id: UUID) async -> Result<Void, MHDataError>
