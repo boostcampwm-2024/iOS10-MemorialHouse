64c488744edb3656415b906dc1a51ffcf60cad48
iceHood <hanyo3477@gmail.com>
2024-11-27 16:49:30 +0900

    refactor: Decodable대신 CoreData사용

--- MemorialHouse/MHData/MHData/LocalStorage/CoreData/CoreDataBookStorage.swift
+++ MemorialHouse/MHData/MHData/LocalStorage/CoreData/CoreDataBookStorage.swift
@@ -87,18 +87,92 @@ extension CoreDataBookStorage {
     // MARK: - Core to DTO
     private func coreBookToDTO(_ book: BookEntity) -> BookDTO? {
         guard let id = book.id,
-              let pages = try? JSONDecoder().decode([PageDTO].self, from: book.pages ?? Data())
+              let corePages = book.pages
         else { return nil }
+        let pages = corePagesToDTO(corePages)
         
         return BookDTO(
             id: id,
             pages: pages
         )
     }
+    private func corePagesToDTO(_ pages: NSOrderedSet) -> [PageDTO] {
+        return pages.compactMap {
+            guard let page = $0 as? PageEntity else { return nil }
+            return corePageToDTO(page)
+        }
+    }
+    private func corePageToDTO(_ page: PageEntity) -> PageDTO? {
+        guard let id = page.id,
+              let mediaDescriptions = page.mediaDescriptions,
+              let text = page.text
+        else { return nil }
+        
+        let metadata = mediaDescriptions.reduce(into: [Int: MediaDescriptionDTO]()) { partialResult, element in
+            guard let element = element as? MediaDescriptionEntity else { return }
+            guard let (index, mediaDescription) = coreMediaDescriptionToDTO(element) else { return }
+            partialResult[index] = mediaDescription
+        }
+                
+        return PageDTO(
+            id: id,
+            metadata: metadata,
+            text: text
+        )
+    }
+    private func coreMediaDescriptionToDTO(_ mediaDescription: MediaDescriptionEntity) -> (Int, MediaDescriptionDTO)? {
+        guard let id = mediaDescription.id,
+              let type = mediaDescription.type
+        else { return nil }
+        let location = mediaDescription.location
+        return (Int(location), MediaDescriptionDTO(
+            id: id,
+            type: type
+        ))
+    }
     
     // MARK: - DTO to Core
-    private func dtoPagesToCore(_ pages: [PageDTO]) -> Data? {
-        guard let data = try? JSONEncoder().encode(pages) else { return nil }
-        return data
+    private func dtoPagesToCore(_ pages: [PageDTO]) -> NSOrderedSet? {
+        let context = coreDataStorage.persistentContainer.viewContext
+        let pageEntities = pages.compactMap { dtoPageToCore($0, in: context) }
+        return NSOrderedSet(array: pageEntities)
     }
+
+    private func dtoPageToCore(_ page: PageDTO, in context: NSManagedObjectContext) -> PageEntity? {
+        guard let entity = NSEntityDescription.insertNewObject(forEntityName: "PageEntity", into: context) as? PageEntity else {
+            return nil
+        }
+        
+        entity.id = page.id
+        entity.text = page.text
+        entity.mediaDescriptions = dtoMediaDescriptionsToCore(page.metadata, in: context)
+        
+        return entity
+    }
+
+    private func dtoMediaDescriptionsToCore(
+        _ metadata: [Int: MediaDescriptionDTO],
+        in context: NSManagedObjectContext
+    ) -> NSSet? {
+        let mediaDescriptionEntities = metadata.compactMap { location, description in
+            dtoMediaDescriptionToCore(location: location, description, in: context)
+        }
+        return NSSet(array: mediaDescriptionEntities)
+    }
+    private func dtoMediaDescriptionToCore(
+        location: Int,
+        _ mediaDescription: MediaDescriptionDTO,
+        in context: NSManagedObjectContext
+    ) -> MediaDescriptionEntity? {
+        guard let entity = NSEntityDescription.insertNewObject(forEntityName: "MediaDescriptionEntity", into: context) as? MediaDescriptionEntity else {
+            return nil
+        }
+        
+        entity.id = mediaDescription.id
+        entity.type = mediaDescription.type
+        entity.location = Int64(location)
+        
+        return entity
+    }
+
 }
