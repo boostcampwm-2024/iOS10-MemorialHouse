c6571b952194f76a26912735bcf127b0c2fc1d6f
iceHood <hanyo3477@gmail.com>
2024-11-28 16:53:45 +0900

    feat: bookViewModel Usecase와 연결

--- MemorialHouse/MHPresentation/MHPresentation/Source/EditBook/ViewModel/EditBookViewModel.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/EditBook/ViewModel/EditBookViewModel.swift
@@ -1,9 +1,14 @@
+import MHFoundation
 import Combine
+import MHDomain
 
 final class EditBookViewModel: ViewModelType {
     // MARK: - Type
     enum Input {
         case viewDidLoad
+        case didAddMediaWithData(type: MediaType, at: Int, data: Data)
+        case didAddMediaInURL(type: MediaType, at: Int, url: URL)
+        case didSaveButtonTapped
     }
     enum Output {
         case setTableView
@@ -12,20 +17,60 @@ final class EditBookViewModel: ViewModelType {
     // MARK: - Property
     private let output = PassthroughSubject<Output, Never>()
     private var cancellables = Set<AnyCancellable>()
-    private var pages = [String]()
+    private let fetchBookUseCase: FetchBookUseCase
+    private let updateBookUseCase: UpdateBookUseCase
+    private let storeBookUseCase: PersistentlyStoreMediaUseCase
+    private let bookID: UUID
+    private var book: Book?
+    
+    // MARK: - Initializer
+    init(
+        fetchBookUseCase: FetchBookUseCase,
+        updateBookUseCase: UpdateBookUseCase,
+        storeBookUseCase: PersistentlyStoreMediaUseCase,
+        bookID: UUID
+    ) {
+        self.fetchBookUseCase = fetchBookUseCase
+        self.updateBookUseCase = updateBookUseCase
+        self.storeBookUseCase = storeBookUseCase
+        self.bookID = bookID
+    }
     
     // MARK: - Method
     func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
         input.sink { [weak self] event in
             switch event {
             case .viewDidLoad:
-                self?.fetchPages()
+                self?.fetchBook()
+            case let .didAddMediaWithData(type, at, data):
+                self?.addMedia(type: type, at: at, with: data)
+            case let .didAddMediaInURL(type, at, url):
+                self?.addMedia(type: type, at: at, in: url)
+            case .didSaveButtonTapped:
+                <#code#>
             }
         }.store(in: &cancellables)
         
         return output.eraseToAnyPublisher()
     }
-    private func fetchPages() {
-        // TODO: - Page가져오는 로직 추가
+    private func fetchBook() {
+        Task {
+            book = try await fetchBookUseCase.execute(bookID: bookID)
+        }
     }
+    private func addMedia(type: MediaType, at index: Int, with data: Data) {
+        let description = MediaDescription(
+            id: UUID(),
+            type: type
+        )
+        
+    }
+    private func addMedia(type: MediaType, at index: Int, in url: URL) {
+        let description = MediaDescription(
+            id: UUID(),
+            type: type
+        )
+        
+    }
+
 }
