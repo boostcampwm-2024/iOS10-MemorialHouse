b86d39d917e13d0f6d3ed8cdd799905ecc100505
iceHood <hanyo3477@gmail.com>
2024-11-28 16:52:41 +0900

    feat: media 타입 설정

--- MemorialHouse/MHDomain/MHDomain/Entity/MediaType.swift
+++ MemorialHouse/MHDomain/MHDomain/Entity/MediaType.swift
@@ -2,4 +2,15 @@ public enum MediaType: String, Sendable {
     case image
     case video
     case audio
+    
+    var defaultFileExtension: String {
+        switch self {
+        case .image:
+            return "png"
+        case .video:
+            return "mp4"
+        case .audio:
+            return "m4a"
+        }
+    }
 }
