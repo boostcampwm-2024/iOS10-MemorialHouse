945cefc40cc01dd6d8e5a061564182c30f7d3ae2
박효준 <gywns626@naver.com>
2024-11-28 16:54:04 +0900

    feat: BookCategory 엔티티와 DTO 추가

---
+++ MemorialHouse/MHDomain/MHDomain/Entity/BookCategory.swift
@@ -0,0 +1,12 @@
+public struct BookCategory {
+    let order: Int
+    let name: String
+    
+    public init(
+        order: Int,
+        name: String
+    ) {
+        self.order = order
+        self.name = name
+    }
+}
