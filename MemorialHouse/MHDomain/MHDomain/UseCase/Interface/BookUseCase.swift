b04333bbfbd6136e38db68280cfdcfab28bcc150
k2645 <k2645@naver.com>
2024-11-30 01:24:23 +0900

    feat: Repository 및 UseCase, Entity Sendable 채택

--- MemorialHouse/MHDomain/MHDomain/UseCase/Interface/BookUseCase.swift
+++ MemorialHouse/MHDomain/MHDomain/UseCase/Interface/BookUseCase.swift
@@ -1,17 +1,17 @@
 import MHFoundation
 
-public protocol CreateBookUseCase {
+public protocol CreateBookUseCase: Sendable {
     func execute(book: Book) async throws
 }
 
-public protocol FetchBookUseCase {
+public protocol FetchBookUseCase: Sendable {
     func execute(id: UUID) async throws -> Book
 }
 
-public protocol UpdateBookUseCase {
+public protocol UpdateBookUseCase: Sendable {
     func execute(id: UUID, book: Book) async throws
 }
 
-public protocol DeleteBookUseCase {
+public protocol DeleteBookUseCase: Sendable {
     func execute(id: UUID) async throws
 }
