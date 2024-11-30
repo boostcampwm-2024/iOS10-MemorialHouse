caf0a51f2a93d245ed69c3daae9791bbcf6a3cc3
박효준 <gywns626@naver.com>
2024-11-30 21:07:31 +0900

    feat: BookCover CRUD 프로토콜 Sendable 추가

--- MemorialHouse/MHDomain/MHDomain/Repository/BookCoverRepository.swift
+++ MemorialHouse/MHDomain/MHDomain/Repository/BookCoverRepository.swift
@@ -1,10 +1,10 @@
 import MHFoundation
 
 // TODO: Result로 변경
-public protocol BookCoverRepository {
     func create(with bookCover: BookCover) async
     func fetchAllBookCovers() async -> [BookCover]
     func fetchBookCover(with id: UUID) async -> BookCover?
     func update(id: UUID, with bookCover: BookCover) async
     func deleteBookCover(_ id: UUID) async
+public protocol BookCoverRepository: Sendable {
 }
