4e1acc2a60d68bd63073495d8e44a9481cae1276
박효준 <gywns626@naver.com>
2024-11-30 21:07:58 +0900

    refactor: BookCover Repo의 CRUD 반환을 Result로 변경

--- MemorialHouse/MHDomain/MHDomain/Repository/BookCoverRepository.swift
+++ MemorialHouse/MHDomain/MHDomain/Repository/BookCoverRepository.swift
@@ -1,10 +1,10 @@
 import MHFoundation
+import MHCore
 
-// TODO: Result로 변경
-    func create(with bookCover: BookCover) async
-    func fetchAllBookCovers() async -> [BookCover]
-    func fetchBookCover(with id: UUID) async -> BookCover?
-    func update(id: UUID, with bookCover: BookCover) async
-    func deleteBookCover(_ id: UUID) async
 public protocol BookCoverRepository: Sendable {
+    func createBookCover(with bookCover: BookCover) async -> Result<Void, MHDataError>
+    func fetchAllBookCovers() async -> Result<[BookCover], MHDataError>
+    func fetchBookCover(with id: UUID) async -> Result<BookCover?, MHDataError>
+    func updateBookCover(id: UUID, with bookCover: BookCover) async -> Result<Void, MHDataError>
+    func deleteBookCover(id: UUID) async -> Result<Void, MHDataError>
 }
