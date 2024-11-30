b1bd383cf3296b51c616f366a418b9d4a7805edd
박효준 <gywns626@naver.com>
2024-11-30 20:45:13 +0900

    chore: CRUD 순서 정렬

--- MemorialHouse/MHDomain/MHDomain/Repository/BookCoverRepository.swift
+++ MemorialHouse/MHDomain/MHDomain/Repository/BookCoverRepository.swift
@@ -2,9 +2,9 @@ import MHFoundation
 
 // TODO: Result로 변경
 public protocol BookCoverRepository {
+    func create(with bookCover: BookCover) async
     func fetchAllBookCovers() async -> [BookCover]
     func fetchBookCover(with id: UUID) async -> BookCover?
+    func update(id: UUID, with bookCover: BookCover) async
     func deleteBookCover(_ id: UUID) async
-    func create(bookCover: BookCover) async
-    func update(id: UUID, bookCover: BookCover) async
 }
