0e27e8dcffa77b80cccf72f415379d0f6ac6b61e
박효준 <gywns626@naver.com>
2024-11-30 21:39:56 +0900

    feat: 홈뷰모델 팩토리에서 책커버 갱신 UseCase 추가

--- MemorialHouse/MHPresentation/MHPresentation/Source/Home/HomeViewModelFactory.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Home/HomeViewModelFactory.swift
@@ -2,14 +2,20 @@ import MHDomain
 
 public struct HomeViewModelFactory {
     let fetchMemorialHouseUseCase: FetchMemorialHouseUseCase
+    let updateBookCoverUseCase: UpdateBookCoverUseCase
     
     public init(
         fetchMemorialHouseUseCase: FetchMemorialHouseUseCase,
+        updateBookCoverUseCase: UpdateBookCoverUseCase
     ) {
         self.fetchMemorialHouseUseCase = fetchMemorialHouseUseCase
+        self.updateBookCoverUseCase = updateBookCoverUseCase
     }
     
     public func make() -> HomeViewModel {
-        HomeViewModel(fetchMemorialHouseUseCase: fetchMemorialHouseUseCase)
+        HomeViewModel(
+            fetchMemorialHouseUseCase: fetchMemorialHouseUseCase,
+            updateBookCoverUseCase: updateBookCoverUseCase
+        )
     }
 }
