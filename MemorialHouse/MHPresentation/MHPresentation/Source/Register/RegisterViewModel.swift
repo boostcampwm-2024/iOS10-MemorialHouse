68c69f6e923825aacb1088d33bf9f694bf185b0c
박효준 <gywns626@naver.com>
2024-12-01 17:56:11 +0900

    feat: RegisterViewModel 로직 구현

--- MemorialHouse/MHPresentation/MHPresentation/Source/Register/RegisterViewModel.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Register/RegisterViewModel.swift
@@ -1,4 +1,5 @@
 import MHFoundation
+import MHDomain
 import Combine
 
 public final class RegisterViewModel: ViewModelType {
@@ -10,20 +11,33 @@ public final class RegisterViewModel: ViewModelType {
     enum Output {
         case registerButtonEnabled(isEnabled: Bool)
         case moveToHome
+        case createFailure(errorMessage: String)
     }
     
+    private let createMemorialHouseNameUseCase: CreateMemorialHouseNameUseCase
     private let output = PassthroughSubject<Output, Never>()
     private var cancellables = Set<AnyCancellable>()
     
-    public init() { }
+    public init(
+        createMemorialHouseNameUseCase: CreateMemorialHouseNameUseCase
+    ) {
+        self.createMemorialHouseNameUseCase = createMemorialHouseNameUseCase
+    }
     
     func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
         input.sink { [weak self] event in
             switch event {
             case .registerTextFieldEdited(let text):
                 self?.validateTextField(text: text)
-            case .registerButtonTapped(let text):
-                self?.registerButtonTapped(text: text)
+            case .registerButtonTapped(let memorialHouseName):
+                Task {
+                    do {
+                        try await self?.registerButtonTapped(with: memorialHouseName)
+                        self?.output.send(.moveToHome)
+                    } catch {
+                        self?.output.send(.createFailure(errorMessage: error.localizedDescription))
+                    }
+                }
             }
         }.store(in: &cancellables)
         
@@ -38,11 +52,7 @@ public final class RegisterViewModel: ViewModelType {
         output.send(.registerButtonEnabled(isEnabled: !text.isEmpty && text.count < 11))
     }
     
-    private func registerButtonTapped(text: String) {
-        UserDefaults.standard.set(
-            text,
-            forKey: Constant.houseNameUserDefaultKey
-        )
-        output.send(.moveToHome)
+    private func registerButtonTapped(with memorialHouseName: String) async throws {
+        try await createMemorialHouseNameUseCase.execute(with: memorialHouseName)
     }
 }
