b6e3f6e05eacdec4e28d5e31d2b1b6ceb554273c
k2645 <k2645@naver.com>
2024-12-01 22:51:09 +0900

    feat: MediaDescription의 Key값 상수 추가

--- MemorialHouse/MHFoundation/MHFoundation/Constant/Constant.swift
+++ MemorialHouse/MHFoundation/MHFoundation/Constant/Constant.swift
@@ -1,4 +1,6 @@
 public enum Constant {
     public static let houseNameUserDefaultKey = "houseName"
     public static let navigationBarHeight: CGFloat = 60
+    public static let photoCaption: String = "photoCaption"
+    public static let photoCreationDate: String = "photoCreationDate"
 }
