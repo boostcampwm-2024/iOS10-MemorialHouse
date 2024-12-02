201c1cfec89ce2511ce2575e9f31a9ab27718ac2
iceHood <hanyo3477@gmail.com>
2024-12-02 10:07:17 +0900

    refactor: MediaAttachment 파일 분리

--- MemorialHouse/MHPresentation/MHPresentation/Source/EditBook/View/MediaAttachment.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/EditBook/View/MediaAttachment.swift
@@ -63,38 +63,3 @@ final class MediaAttachment: NSTextAttachment {
         view.configureSource(with: mediaDescription, url: url)
     }
 }
-
-class MediaAttachmentViewProvider: NSTextAttachmentViewProvider {
-    // MARK: - Property
-    var type: MediaType?
-    private var height: CGFloat {
-        switch type { // TODO: - 조정 필요
-        case .image:
-            300
-        case .video:
-            200
-        case .audio:
-            100
-        case nil:
-            10
-        default:
-            100
-        }
-    }
-    
-    override func attachmentBounds(
-        for attributes: [NSAttributedString.Key: Any],
-        location: NSTextLocation,
-        textContainer: NSTextContainer?,
-        proposedLineFragment: CGRect,
-        position: CGPoint
-    ) -> CGRect {
-        return CGRect(x: 0, y: 0, width: proposedLineFragment.width, height: height)
-    }
-}
-
-// MARK: - MediaAttachable
-protocol MediaAttachable {
-    func configureSource(with mediaDescription: MediaDescription, data: Data)
-    func configureSource(with mediaDescription: MediaDescription, url: URL)
-}
