deb0c66ecb5ee71c959596e3c8679137b0a32b2d
Kyxxn <gywns626@naver.com>
2024-12-04 17:18:50 +0900

    fix: 기록소 닉네임 등록할 때, 10글자 제한

--- MemorialHouse/MHPresentation/MHPresentation/Source/Register/View/RegisterViewController.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Register/View/RegisterViewController.swift
@@ -179,8 +179,21 @@ public final class RegisterViewController: UIViewController {
 }
 
 extension RegisterViewController: UITextFieldDelegate {
-    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    public func textFieldShouldReturn(
+        _ textField: UITextField
+    ) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    public func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+    ) -> Bool {
+        guard let currentText = textField.text as NSString? else { return false }
+        let newLength = currentText.length + string.count - range.length
+        
+        return newLength <= 10
     }
 }
