0188cd935b0ed78c2d58bef8d0fd815853db2378
k2645 <k2645@naver.com>
2024-12-05 21:48:26 +0900

    fix: 테스트 코드 수정

--- MemorialHouse/MHPresentation/MHPresentationTests/TestDoubles/StubBookCoverUseCase.swift
+++ MemorialHouse/MHPresentation/MHPresentationTests/TestDoubles/StubBookCoverUseCase.swift
@@ -8,7 +8,7 @@ struct StubFetchAllBookCoverUseCase: FetchAllBookCoverUseCase {
                 id: UUID(uuidString: "11111111-1111-1111-1111-111111111111")!,
                 order: 0,
                 title: "title1",
-                imageURL: nil,
+                imageData: nil,
                 color: .blue,
                 category: "친구",
                 favorite: false
@@ -17,7 +17,7 @@ struct StubFetchAllBookCoverUseCase: FetchAllBookCoverUseCase {
                 id: UUID(uuidString: "22222222-2222-2222-2222-222222222222")!,
                 order: 1,
                 title: "title2",
-                imageURL: nil,
+                imageData: nil,
                 color: .blue,
                 category: "가족",
                 favorite: false
@@ -26,7 +26,7 @@ struct StubFetchAllBookCoverUseCase: FetchAllBookCoverUseCase {
                 id: UUID(uuidString: "33333333-3333-3333-3333-333333333333")!,
                 order: 2,
                 title: "title3",
-                imageURL: nil,
+                imageData: nil,
                 color: .green,
                 category: "전체",
                 favorite: false
@@ -37,4 +37,8 @@ struct StubFetchAllBookCoverUseCase: FetchAllBookCoverUseCase {
 
 struct StubUpdateBookCoverUseCase: UpdateBookCoverUseCase {
     func execute(id: UUID, with bookCover: BookCover) async throws { }
+}
+
+struct StubDeleteBookCoverUseCase: DeleteBookCoverUseCase {
+    func execute(id: UUID) async throws { }
 }
