57cf687de7f4011e58ae3657b81beae2d9757109
박효준 <gywns626@naver.com>
2025-02-27 23:12:54 +0900

    fix: 테스트 파일 의존성 재구성

--- MemorialHouse/MHData/MHDataTests/CoreDataBookCoverStorageTests.swift
+++ MemorialHouse/MHData/MHDataTests/CoreDataBookCoverStorageTests.swift
@@ -2,7 +2,7 @@ import Testing
 @testable import MHCore
 @testable import MHData
 @testable import MHDomain
-@testable import Foundation
+import Foundation
 
 struct CoreDataBookCoverStorageTests {
     // MARK: - Properties
