8f6aaed178856f90ccf28a218fda0c19191e81aa
iceHood <hanyo3477@gmail.com>
2025-03-04 20:59:15 +0900

    fix: 하나만 재생되도록 수정

--- MemorialHouse/MHPresentation/MHPresentation/Source/Design/MHVideoView.swift
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Design/MHVideoView.swift
@@ -5,27 +5,44 @@ import MHDomain
 final class MHVideoView: UIView {
     // MARK: - Property
     let playerViewController = AVPlayerViewController()
+    private var timeControlStatusObservation: NSKeyValueObservation?
 
     // MARK: - Initializer
     init() {
         super.init(frame: .zero)
         configureConstraint()
+        NotificationCenter.default.addObserver(self, selector: #selector(stopPlayer), name: .mediaPlaybackStarted, object: nil)
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         configureConstraint()
+        NotificationCenter.default.addObserver(self, selector: #selector(stopPlayer), name: .mediaPlaybackStarted, object: nil)
     }
     
     // MARK: - Configuration
     func configurePlayer(player: AVPlayer) {
         playerViewController.player = player
         playerViewController.showsPlaybackControls = true
+        timeControlStatusObservation = player.observe(\.timeControlStatus, options: [.new]) { player, change in
+            switch player.timeControlStatus {
+            case .playing:
+                NotificationCenter.default.post(name: .mediaPlaybackStarted, object: self)
+            default:
+                break
+            }
+        }
     }
     
     private func configureConstraint() {
         addSubview(playerViewController.view)
         playerViewController.view.fillSuperview()
+    }
+    
+    @objc
+    private func stopPlayer(_ notification: NSNotification) {
+        guard notification.object as? MHVideoView !== self else { return }
+        playerViewController.player?.pause()
     }
     
     // MARK: - LifeCycle
@@ -35,6 +52,10 @@ final class MHVideoView: UIView {
         if window == nil {
             playerViewController.player?.pause()
         }
+    }
+    
+    deinit {
+        timeControlStatusObservation?.invalidate()
     }
 }
 
---
+++ MemorialHouse/MHPresentation/MHPresentation/Source/Extensions/Notification+MediaPlayback.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+extension Notification.Name {
+    static let mediaPlaybackStarted = Notification.Name("mediaPlaybackStarted")
+}
